f5c2a8047c8c278d3bfed927c04a70af151af4fe
aziyatdin0v <azat.ziyatdinov@yandex.ru>
2022-09-13 21:16:17 +0300

    another conflict

--- src/test/java/guru/qa/ExampleTest.java
+++ src/test/java/guru/qa/ExampleTest.java
@@ -10,11 +10,11 @@ public class ExampleTest {
 
     @Test
     void exampleTest0(){
-        Assertions.assertTrue(2 >= 1);
+        Assertions.assertTrue(2 >= 10);
     }
 
     @Test
     void exampleTest1(){
-        Assertions.assertTrue(3 >= 2);
+        Assertions.assertTrue(3 >= 20);
     }
 }
